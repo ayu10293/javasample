057a2e0f2519e80ca1f671ad7f367ff09d640c90
ayu10293 <snof11513@gmail.com>
2024-11-06 13:26:11 +0900

    mismatchは最初に違う部分を見つけてインデックス番号を返す。同じなら－１を返す。compareはどちらが大きいかを返す。同じなら０を返す。(a,b)だったらaはbよりどのくらい大きいか。aがbより小さかったら－になる。

---
+++ silver/src/exa/X3.java
@@ -0,0 +1,11 @@
+package exa;//48
+import java.util.Arrays;
+public class X3 {
+	public static void main(String[] args) {
+		int[] a = {2, 4, 5, 6, 9};
+		int[] b = {2, 4, 6, 8, 9};
+		System.out.println(Arrays.mismatch(a,  b) + " ");
+		System.out.println(Arrays.compare(a,  b));
+		}
+
+}
