cfd67b8f01f8236eb12a903f8909c7b5e0eadd23
ayu10293 <snof11513@gmail.com>
2024-11-06 14:17:45 +0900

    よくわからん

--- silver/src/exa/X4.java
+++ silver/src/exa/X4.java
@@ -3,13 +3,13 @@ package exa;//49
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class X4 {
+ class XX4 {
 	<T> Collection<T> foo(Collection<T> c) {
 		return c;
 	}
 }
 
-class Child extends X4 {
+class Child extends XX4 {
 	<T> Collection<T> foo(Collection<T> c) {
 		return c;
 	}
@@ -18,13 +18,13 @@ class Child extends X4 {
 	//4<T> Collection<T> foo(Map c){return null;}
 }
 
- class Main2 {
+ class X4 {
 	public static void main(String[] args) {
 		Collection<String> c = new ArrayList<>();
 		c.add("1");
 		c.add("2");
 		System.out.println(c);
-		X4 x4 = new X4();
+		XX4 x4 = new XX4();
 		Collection <String> c2 = x4.foo(c);
 		System.out.println(c2);
 		
