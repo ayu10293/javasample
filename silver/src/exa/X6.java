2bbf4b258da430604dce2f14674a2dd334c1b268
ayu10293 <snof11513@gmail.com>
2024-11-11 13:58:32 +0900

    appendメソッドはStringBuilderに文字列や他のデータを追加,toStringメソッドはStringBuilderの現在の内容をString型で返します。

---
+++ silver/src/exa/X6.java
@@ -0,0 +1,18 @@
+package exa;
+class Foo{
+	int a = 10;
+	void methodA(Integer i) {a += i;}
+	void methodB(StringBuilder sb) {sb.append(" " + sb);}
+}
+
+public class X6 {
+	public static void main(String[] args) {
+		Foo obj = new Foo();
+		int val = 10;
+		obj.methodA(val);
+		StringBuilder sb = new StringBuilder("hello");
+		obj.methodB(sb);
+		System.out.println(val + " : " + sb.toString());
+	}
+
+}//appendメソッドはStringBuilderに文字列や他のデータを追加,toStringメソッドはStringBuilderの現在の内容をString型で返します。
